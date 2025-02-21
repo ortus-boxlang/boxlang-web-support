a7e18e91b045996cfa9cfb40ba4850a09bfe9630
Brad Wood <brad@bradwood.com>
2025-02-21 16:02:17 -0600

    BL-1088

--- src/main/java/ortus/boxlang/web/scopes/CookieScope.java
+++ src/main/java/ortus/boxlang/web/scopes/CookieScope.java
@@ -17,10 +17,18 @@
  */
 package ortus.boxlang.web.scopes;
 
+import java.util.Date;
+
 import ortus.boxlang.runtime.context.IBoxContext;
+import ortus.boxlang.runtime.dynamic.casters.BooleanCaster;
+import ortus.boxlang.runtime.dynamic.casters.CastAttempt;
+import ortus.boxlang.runtime.dynamic.casters.DateTimeCaster;
+import ortus.boxlang.runtime.dynamic.casters.IntegerCaster;
 import ortus.boxlang.runtime.dynamic.casters.StringCaster;
+import ortus.boxlang.runtime.dynamic.casters.StructCaster;
 import ortus.boxlang.runtime.scopes.BaseScope;
 import ortus.boxlang.runtime.scopes.Key;
+import ortus.boxlang.runtime.types.IStruct;
 import ortus.boxlang.web.exchange.BoxCookie;
 import ortus.boxlang.web.exchange.IBoxHTTPExchange;
 
@@ -34,7 +42,10 @@ public class CookieScope extends BaseScope {
 	 * Public Properties
 	 * --------------------------------------------------------------------------
 	 */
-	public static final Key		name	= Key.of( "cookie" );
+	public static final Key		name			= Key.of( "cookie" );
+	private static final Key	maxAgeKey		= Key.of( "maxAge" );
+	private static final Key	sameSiteKey		= Key.of( "sameSite" );
+	private static final Key	sameSiteModeKey	= Key.of( "sameSiteMode" );
 
 	protected IBoxHTTPExchange	exchange;
 
@@ -66,9 +77,70 @@ public class CookieScope extends BaseScope {
 	 */
 	@Override
 	public Object assign( IBoxContext context, Key key, Object value ) {
-		String castedValue = StringCaster.cast( value );
-		this.put( key, castedValue );
-		exchange.addResponseCookie( new BoxCookie( key.getName(), castedValue ) );
+		CastAttempt<IStruct>	structAttempt	= StructCaster.attempt( value );
+		String					cookieValue		= "";
+		String					path			= "/";
+		String					domain			= null;
+		Integer					maxAge			= null;
+		Date					expires			= null;
+		boolean					secure			= false;
+		boolean					httpOnly		= false;
+		int						version			= 0;
+		boolean					sameSite		= false;
+		String					sameSiteMode	= null;
+
+		if ( structAttempt.wasSuccessful() ) {
+			IStruct cookieData = structAttempt.get();
+			if ( cookieData.containsKey( Key.value ) ) {
+				cookieValue = StringCaster.cast( cookieData.get( Key.value ) );
+			}
+			if ( cookieData.containsKey( Key.path ) ) {
+				path = StringCaster.cast( cookieData.get( Key.path ) );
+			}
+			if ( cookieData.containsKey( Key.domain ) ) {
+				domain = StringCaster.cast( cookieData.get( Key.domain ) );
+			}
+			if ( cookieData.containsKey( maxAgeKey ) ) {
+				maxAge = IntegerCaster.cast( cookieData.get( maxAgeKey ) );
+			}
+			if ( cookieData.containsKey( Key.expires ) ) {
+				expires = Date.from( DateTimeCaster.cast( cookieData.get( Key.expires ) ).toInstant() );
+			}
+			if ( cookieData.containsKey( Key.secure ) ) {
+				secure = BooleanCaster.cast( cookieData.get( Key.secure ) );
+			}
+			if ( cookieData.containsKey( Key.httpOnly ) ) {
+				httpOnly = BooleanCaster.cast( cookieData.get( Key.httpOnly ) );
+			}
+			if ( cookieData.containsKey( Key.version ) ) {
+				version = IntegerCaster.cast( cookieData.get( Key.version ) );
+			}
+			if ( cookieData.containsKey( sameSiteKey ) ) {
+				sameSite = BooleanCaster.cast( cookieData.get( sameSiteKey ) );
+			}
+			if ( cookieData.containsKey( sameSiteModeKey ) ) {
+				sameSiteMode = StringCaster.cast( cookieData.get( sameSiteModeKey ) );
+			}
+
+		} else {
+			// Anything other than a struct or string will errror ere
+			cookieValue = StringCaster.cast( value );
+		}
+
+		this.put( key, cookieValue );
+		// If the incoming value was just a struct, most of these will just be defaults
+		exchange.addResponseCookie(
+		    new BoxCookie( key.getName(), cookieValue )
+		        .setPath( path )
+		        .setDomain( domain )
+		        .setMaxAge( maxAge )
+		        .setSecure( secure )
+		        .setVersion( version )
+		        .setHttpOnly( httpOnly )
+		        .setExpires( expires )
+		        .setSameSite( sameSite )
+		        .setSameSiteMode( sameSiteMode )
+		);
 		return value;
 	}
 }
